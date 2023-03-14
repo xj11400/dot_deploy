a51f615b835a186db8be2d65dff2e9fe0ad958a6
XJ Hsu <xj11400@gmail.com>
2023-03-14 23:14:12 +0800

    <feat>(commit): commit.sh

---
+++ commit.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+function submodule_commit() {
+    if [ ! -z "$(git status | grep -E "modified:.+core")" ]; then
+        git add core
+        git commit -m "<submodule>(core): $(git diff --staged core | grep index | cut -d ' ' -f 2)"
+    fi
+}
+
+#submodule_commit
+
+NEW=$(git status | grep -E "modified:.+\(new commits\)" | awk '{print $2}')
+for update in $NEW;do
+    echo $update
+    git add $update
+    git commit -m "<$update>: $(git diff --staged $update | grep index | cut -d ' ' -f 2)"
+done
+
