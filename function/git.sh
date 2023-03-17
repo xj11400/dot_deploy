b4e1a4aedf92a765d362b335b8e947d118ce1229
XJ Hsu <xj11400@gmail.com>
2023-03-18 03:31:45 +0800

    <feat>(func/git): _upvar

--- function/git.sh
+++ function/git.sh
@@ -5,33 +5,37 @@ git_config_input() {
     GIT_USER_NAME="XJ Hsu"
     GIT_USER_EMAIL="xj11400@gmail.com"
 
+    local _git_user_name=$(eval echo \$$1)
+    local _git_user_email=$(eval echo \$$2)
+
     echo ""
     # set user name
-    read -p "enter user name($GIT_USER_NAME): " gitUserName
+    read -p "enter user name($GIT_USER_NAME): " _git_user_name
 
-    if [ -z $gitUserName ]; then
-        echo " >>> user.name = $GIT_USER_NAME"
-    else
-        echo " >>> user.name = $gitUserName"
-        GIT_USER_NAME=$gitUserName
+    if [ -z $_git_user_name ]; then
+        _git_user_name=$GIT_USER_NAME
     fi
 
-    git config --global user.name "$GIT_USER_NAME"
+    echo " >>> user.name = $_git_user_name"
+
+    git config --global user.name "$_git_user_name"
 
     echo ""
     # set user email
-    read -p "enter user email($GIT_USER_EMAIL): " gitUserEmail
+    read -p "enter user email($GIT_USER_EMAIL): " _git_user_email
 
-    if [ -z $gitUserEmail ]; then
-        echo " >>> user.email = $GIT_USER_EMAIL"
-    else
-        echo " >>> user.email = $gitUserEmail"
-        GIT_USER_EMAIL=$gitUserEmail
+    if [ -z $_git_user_email ]; then
+        _git_user_email=$GIT_USER_EMAIL
     fi
 
-    git config --global user.email "$GIT_USER_EMAIL"
+    echo " >>> user.email = $_git_user_email"
+
+    git config --global user.email "$_git_user_email"
 
     echo ""
+
+    local "$1" && _upvar $1 "${_git_user_name}"
+    local "$2" && _upvar $2 "${_git_user_email}"
 }
 
 git_config() {
@@ -56,23 +60,31 @@ git_write_conf_path() {
 }
 
 git_check_config() {
-    if [[ ! -f $GIT_CONFIG_FILE ]]; then
+    local _user_name=$(eval echo \$$1)
+    local _user_email=$(eval echo \$$2)
+
+    if [ ! -f $GIT_CONFIG_FILE ]; then
         echo " >>> not found git config file..."
         echo " >>> touch $GIT_CONFIG_FILE ..."
         touch $GIT_CONFIG_FILE
 
         # setting user.name and user.email
-        if [ -z $1 ] || [ -z $2 ]; then
+        if [ -z $_user_name ] || [ -z $_user_email ]; then
             echo "missing arg"
-            git_config_input
+            git_config_input _user_name _user_email
         else
-            git_config $1 $2
+            git_config _user_name _user_email
         fi
     else
         echo " >>> found git config file..."
+        _user_name="$(git config --global user.name)"
+        _user_email="$(git config --global user.email)"
     fi
+
+    local "$1" && _upvar $1 $_user_name
+    local "$2" && _upvar $2 $_user_email
 }
 
 #
-git_check_config $@
-git_write_conf_path
+# git_check_config $@
+# git_write_conf_path
